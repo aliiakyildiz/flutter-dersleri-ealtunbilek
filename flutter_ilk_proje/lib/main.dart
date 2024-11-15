34dda58539b20f2b7959c842353ed18e8d9c5aef
Ali Akyıldız <ali.akyildiz.94@gmail.com>
2024-11-15 11:00:44 +0300

    Theme settings, body, text style ...

--- flutter_ilk_proje/lib/main.dart
+++ flutter_ilk_proje/lib/main.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const FirstApp());
 }
 
-class MyApp extends StatelessWidget {
+class FirstApp extends StatelessWidget {
+  const FirstApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Colors.teal),
+        scaffoldBackgroundColor: Colors.grey[100],
+      ),
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.amber,
           title: const Text("AppBar"),
         ),
-        body: const Text("Hello Word"),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("Tıklandı");
+          },
+          backgroundColor: Colors.red,
+          child: const Icon(
+            Icons.access_time,
+            color: Colors.cyan,
+          ),
+        ),
+        body: const Text(
+          "Hello Word",
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
     );
   }
